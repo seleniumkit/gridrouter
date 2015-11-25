acdbc06845127e27bade75db7cb56cb7c4a7855a
innokenty <innokenty@yandex-team.ru>
2015-11-25 20:22:54 +0300

    add getHosts method to Browsers bean

--- config/src/main/java/ru/qatools/gridrouter/config/WithRoutesMap.java
+++ config/src/main/java/ru/qatools/gridrouter/config/WithRoutesMap.java
@@ -1,24 +1,17 @@
 package ru.qatools.gridrouter.config;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
  * @author Dmitry Baev charlie@yandex-team.ru
  * @author Innokenty Shuvalov innokenty@yandex-team.ru
  */
-public interface WithRoutesMap {
-
-    List<Browser> getBrowsers();
+public interface WithRoutesMap extends WithHosts {
 
     default Map<String, String> getRoutesMap() {
-        HashMap<String, String> routes = new HashMap<>();
-        getBrowsers().stream()
-                .flatMap(b -> b.getVersions().stream())
-                .flatMap(v -> v.getRegions().stream())
-                .flatMap(r -> r.getHosts().stream())
-                .forEach(h -> routes.put(h.getRouteId(), h.getRoute()));
+        Map<String, String> routes = new HashMap<>();
+        getHosts().forEach(h -> routes.put(h.getRouteId(), h.getRoute()));
         return routes;
     }
 }
